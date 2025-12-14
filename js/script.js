8a8a205c079d7c6b3a004f33e58e72191b6e6f16
Ananya9794 <sethiananya99@gmail.com>
2025-12-15 02:32:33 +0530

    first

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 2500,
+      responsive:{
+        0:{ items:1 },
+        600:{ items:2 },
+        1000:{ items:3 }
+      }
+    });
